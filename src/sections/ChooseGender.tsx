084503fc63882805fced0e22ab73fee8c78f31be
Jahon Mirzaakhmedov <jokhuzb@mail.ru>
2024-10-09 11:14:46 +0500

    sdf

--- src/sections/ChooseGender.tsx
+++ src/sections/ChooseGender.tsx
@@ -1,30 +1,30 @@
-import desktopAndPhoneLeft from "@/assets/desktop-phone-left.png";
+import desktopAndPhoneLeft from "@/assets/noutbook.png";
 
 const ChooseGender = () => {
-  return (
-    <div className="bg-[#F6F6F6] w-full py-[17px] flex justify-between items-center gap-[70px]">
-      <div>
-        <img src={desktopAndPhoneLeft} alt="desktop and phone image" />
-      </div>
-      <div className="flex flex-col items-start justify-start gap-[85px]  ">
-        <div className="justify-start items-start gap-[17px] flex-col max-w-[500px]">
-          <h2 className="text-3xl lg:text-4xl font-[500] text-start">
-            Определение жанра композиции
-          </h2>
-          <p className=" text-[16px]  text-start">
-            Отправьте песню и узнайте её жанр с помощью нашего продвинутого
-            анализа звука.
-          </p>
-        </div>
-        <a
-          href="/"
-          className="block text-white py-2 px-4 bg-[#0F172A] rounded-[6px]"
-        >
-          Продолжить
-        </a>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-[#F6F6F6] w-full py-[17px] flex justify-between items-center gap-[70px]">
+			<div>
+				<img src={desktopAndPhoneLeft} alt="desktop and phone image" />
+			</div>
+			<div className="flex flex-col items-start justify-start gap-[85px]  ">
+				<div className="justify-start items-start gap-[17px] flex-col max-w-[500px]">
+					<h2 className="text-3xl lg:text-4xl font-[500] text-start">
+						Определение жанра композиции
+					</h2>
+					<p className=" text-[16px]  text-start">
+						Отправьте песню и узнайте её жанр с помощью нашего продвинутого
+						анализа звука.
+					</p>
+				</div>
+				<a
+					href="/"
+					className="block text-white py-2 px-4 bg-[#0F172A] rounded-[6px]"
+				>
+					Продолжить
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default ChooseGender;
--- src/sections/SearchMovie.tsx
+++ src/sections/SearchMovie.tsx
@@ -1,31 +1,31 @@
 import desktopAndPhoneRight from "@/assets/desktop-phone-right.png";
 
 const SearchMovie = () => {
-  return (
-    <div className="bg-[#F6F6F6] w-full py-[17px] flex justify-between items-center gap-[70px]">
-      <div className="flex flex-col items-end justify-end gap-[85px]  ">
-        <div className=" justify-end items-start gap-[17px] flex-col max-w-[500px]">
-          <h2 className="text-3xl lg:text-4xl font-[500] text-end">
-            Поиск фильмов по сценам или типу
-          </h2>
-          <p className=" text-[16px]  text-end">
-            Найдите фильм, который помните по сцене или выберите тип фильма.
-            TopMusicBot предоставит вам список фильмов, соответствующих вашим
-            запросам.
-          </p>
-        </div>
-        <a
-          href="/"
-          className="block text-white py-2 px-4 bg-[#0F172A] rounded-[6px]"
-        >
-          Продолжить
-        </a>
-      </div>
-      <div>
-        <img src={desktopAndPhoneRight} alt="desktop and phone image" />
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-[#F6F6F6] w-full py-[17px] flex justify-between items-center gap-[70px]">
+			<div className="flex flex-col items-center mx-auto justify-center gap-[85px]    ">
+				<div className=" justify-end items-start gap-[17px] flex-col max-w-[500px]">
+					<h2 className="text-3xl lg:text-4xl font-[500] text-end">
+						Поиск фильмов по сценам или типу
+					</h2>
+					<p className=" text-[16px]  text-end">
+						Найдите фильм, который помните по сцене или выберите тип фильма.
+						TopMusicBot предоставит вам список фильмов, соответствующих вашим
+						запросам.
+					</p>
+				</div>
+				<a
+					href="/"
+					className="block text-white py-2 px-4 bg-[#0F172A] rounded-[6px]"
+				>
+					Продолжить
+				</a>
+			</div>
+			<div>
+				<img src={desktopAndPhoneRight} alt="desktop and phone image" />
+			</div>
+		</div>
+	);
 };
 
 export default SearchMovie;
